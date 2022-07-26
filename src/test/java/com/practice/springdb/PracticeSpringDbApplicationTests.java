cc7a7b6f3355817fdb1b92777c0445e3bd857fa4
Jobanjit Singh <jbnjeet@gmail.com>
2022-07-26 10:16:06 +0530

    first commit

---
+++ src/test/java/com/practice/springdb/PracticeSpringDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
